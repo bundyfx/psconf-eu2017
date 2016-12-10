aca885a9b39770a5b5b817a15d4f1ad8a2e5987c
Flynn Bundy <f.bundy@coolblue.nl>
2016-12-10 18:46:02 +0100

    pushing up beer app to use as part of demo

--- windows-server-containers/demo/src/routes/search.js
+++ windows-server-containers/demo/src/routes/search.js
@@ -1,16 +1,21 @@
 const util = require('util');
-var request = require('request');
 
-var userinput = 'heineken'
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
 
-function create_querystring(userinput){
-  var api = util.format('http://api.brewerydb.com/v2/search?q=%s&type=beer&key=6b757c6f59edd9a3eabae917422ee8a4', userinput);
-  return api
+var key = process.env.apikey
+
+module.exports = {
+
+  create_query: function (userinput, callback){
+  var api = util.format('http://api.brewerydb.com/v2/search?q=%s&type=beer&key=%s', userinput, key);
+
+  request(api, function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+     var obj = JSON.parse(body)
+     callback(obj);
+   }
+  })
+ }
 }
-
-request(create_querystring(userinput), function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var obj = JSON.parse(body)
-    console.log(obj)
-  }
-})
