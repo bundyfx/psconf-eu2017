9b11164c6effdf140785f6027b20286bc3f110c5
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-14 19:16:58 +0100

    Fix pathing

--- developers-world/demo/Tests/Integration/Integration.ps1
+++ developers-world/demo/Tests/Integration/Integration.ps1
@@ -1,4 +1,10 @@
+#Store out current location
+Push-Location
+
+#Change direcotry to the directory with our COnfiguration Data
 Set-Location .\developers-world\demo\ConfigurationData\
+
+#Import that Hashtable data in with LocalizedData
 Import-LocalizedData -BaseDirectory . -FileName ConfigurationData.psd1 -ov ConfigurationData | Out-Null
 
 #Ensure Configuration Data is Valid
@@ -10,3 +16,6 @@ Describe "Valid Configuration Data" {
         $ConfigurationData.Values.Keys.Where{$Psitem -eq 'Role'}.Count | Should be $ConfigurationData.Values.Length
     }
 }
+
+#Return back to the pushed location
+Pop-Location
