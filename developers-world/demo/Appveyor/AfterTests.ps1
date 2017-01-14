6adb7b6015ac43803cea9162f4ffcdddd6eacbd1
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-14 16:22:39 +0100

    Update Config Data pathing

--- developers-world/demo/Appveyor/AfterTests.ps1
+++ developers-world/demo/Appveyor/AfterTests.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = 'Stop'
 
  Import-Module '.\developers-world\demo\Generation\Mof\ConfigurationScript.ps1'
- Main -ConfigurationData '..\ConfigurationData\ConfigurationData.ps1' -OutputPath $PscScriptRoot
+ Main -ConfigurationData '.\developers-world\demo\ConfigurationData\ConfigurationData.ps1' -OutputPath $PscScriptRoot
