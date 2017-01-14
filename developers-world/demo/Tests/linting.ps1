a4e0eab713f8f8979763395500ef7ebe421cbb8c
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-14 13:55:21 +0100

    Updating Linting

--- developers-world/demo/Tests/linting.ps1
+++ developers-world/demo/Tests/linting.ps1
@@ -3,7 +3,7 @@ $Rules = Get-ScriptAnalyzerRule
 Describe "Linting all .ps1 files" {
     foreach ($module in $modules) {
         Context "Testing Module | $($module.BaseName) for Standard Processing" {
-            foreach ($rule in $rules) {
+            foreach ($rule in $rules.Where{$Psitem.Rulename -ne 'PSAvoidUsingPositionalParameters'}) {
                 It "Passes rule | $($Rule.rulename)" {
                     (Invoke-ScriptAnalyzer -Path $module.FullName -IncludeRule $rule.RuleName).Count | Should Be 0
                 }
