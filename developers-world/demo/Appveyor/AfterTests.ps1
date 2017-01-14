9f03a75775a9febf3b0b9a170d95447d6ae47dd7
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-14 17:43:14 +0100

    Testing S3 Deployment

--- developers-world/demo/Appveyor/AfterTests.ps1
+++ developers-world/demo/Appveyor/AfterTests.ps1
@@ -1,4 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
+#Create a new folder for Outputing the Mofs
+$OutputPath = New-Item -Path (Join-Path $PSScriptRoot -ChildPath Output) -ItemType Directory
+
+#Import the Configuration Script
 Import-Module '.\developers-world\demo\Generation\Mof\ConfigurationScript.ps1'
-Main -OutputPath $PSScriptRoot -ConfigurationData '.\developers-world\demo\ConfigurationData\ConfigurationData.psd1'
+
+#Call The Configuration Script passing in our Configuration Data
+Main -OutputPath $OutputPath.Fullname -ConfigurationData '.\developers-world\demo\ConfigurationData\ConfigurationData.psd1'
+
+#Create a Zip from the Mof output
+Compress-Archive '.\developers-world\demo\Generation\Mof\Output' -DestinationPath '.\developers-world\demo\Generation\Mof\Output\Mofs.zip' -Verbose
