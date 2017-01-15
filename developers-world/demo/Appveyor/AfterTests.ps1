09e7ca7d04462bfe049139de5edc67df75d7a903
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-15 17:28:14 +0100

    Add xPSDSC

--- developers-world/demo/Appveyor/AfterTests.ps1
+++ developers-world/demo/Appveyor/AfterTests.ps1
@@ -5,4 +5,4 @@ Import-Module '.\developers-world\demo\Generation\Mof\ConfigurationScript.ps1'
 Main -OutputPath $ProjectRoot -ConfigurationData '.\developers-world\demo\ConfigurationData\ConfigurationData.psd1'
 
 #Create a Zip from the Mof output (Include Modules)
-Get-Childitem -Path "$ProjectRoot\*.mof","$ProjectRoot\cChoco","$ProjectRoot\xPSDesiredStateConfiguration" -Recurse | Compress-Archive -DestinationPath $ProjectRoot -Verbose
+Get-Childitem -Path "$ProjectRoot\yumtop.mof","$ProjectRoot\gopher-world.mof","$ProjectRoot\cChoco","$ProjectRoot\xPSDesiredStateConfiguration" -Recurse | Compress-Archive -DestinationPath $ProjectRoot`.zip -Verbose
