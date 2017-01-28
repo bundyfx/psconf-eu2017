ef0e9fbb1837467175b66e6723c94db0531dec12
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-28 15:39:21 +0100

    Updating Userdata

--- developers-world/demo/Cloudformation/EC2Userdata.ps1
+++ developers-world/demo/Cloudformation/EC2Userdata.ps1
@@ -13,18 +13,13 @@ netsh advfirewall firewall add rule name="Open Port 5000" dir=in action=allow pr
 #Apply DSC Configuration
 Start-DscConfiguration -path C:\windows\temp\ -Verbose -wait -force -ComputerName localhost
 
-Set-Alias -name git -value 'C:\Program Files\Git\bin\git.exe'
-Set-Alias -name npm -value 'C:\Program Files\nodejs\npm.cmd'
-
 #Datadog install
-& msiexec /qn /i C:\Windows\Temp\datadog\ddagent-cli.msi APIKEY="$Env:datadogAPIkey" HOSTNAME="$Env:ComputerName" TAGS="mytag1,mytag2"
-
-refreshenv
+& msiexec /qn /i C:\Windows\Temp\datadog\ddagent-cli.msi APIKEY=$($Env:datadogAPIkey) HOSTNAME=$($Env:ComputerName) TAGS="mytag1,mytag2"
 
 #Clone repo and install node modules
-start-process git -ArgumentList "clone https://github.com/bundyfx/psconf-eu2017-beertime.git" -Wait
+start-process "C:\Program Files\Git\bin\git.exe" -ArgumentList "clone https://github.com/bundyfx/psconf-eu2017-beertime.git" -Wait
 Set-Location C:\App\psconf-eu2017-beertime
-start-process npm -ArgumentList "install" -Wait
+start-process "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install" -Wait
 
 #Stop all transcription
 Stop-Transcript
@@ -32,6 +27,6 @@ Stop-Transcript
 #Write transcription to S3
 Write-S3Object -BucketName powershell-dsc-mofs -File C:\Userdata_$Timestamp.txt
 
-start-process npm start
+start-process "C:\Program Files\nodejs\npm.cmd" -ArgumentList "start" -Wait
 
 exit 0
