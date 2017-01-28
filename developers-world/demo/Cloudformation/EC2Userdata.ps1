56dfbb372c4f2711e16741926e3be22a34b76cf5
Flynn Bundy <flynn.sbundy@gmail.com>
2017-01-28 16:36:28 +0100

    userdata

--- developers-world/demo/Cloudformation/EC2Userdata.ps1
+++ developers-world/demo/Cloudformation/EC2Userdata.ps1
@@ -14,11 +14,11 @@ netsh advfirewall firewall add rule name="Open Port 5000" dir=in action=allow pr
 Start-DscConfiguration -path C:\windows\temp\ -Verbose -wait -force -ComputerName localhost
 
 #Datadog install
-& msiexec /qn /i C:\Windows\Temp\datadog\ddagent-cli.msi APIKEY=$($Env:datadogAPIkey) HOSTNAME=$($Env:ComputerName) TAGS="mytag1,mytag2"
+cmd.exe /c msiexec /qn /i C:\Windows\Temp\datadog\ddagent-cli.msi APIKEY=$($Env:datadogAPIkey) HOSTNAME=$($Env:ComputerName) TAGS="mytag1,mytag2"
 
 #Clone repo and install node modules
-start-process "C:\Program Files\Git\bin\git.exe" -ArgumentList "clone https://github.com/bundyfx/psconf-eu2017-beertime.git" -Wait
-Set-Location C:\App\psconf-eu2017-beertime
+start-process "C:\Program Files\Git\bin\git.exe" -ArgumentList "clone https://github.com/bundyfx/psconf-eu2017-beertime.git C:\App" -Wait
+Set-Location C:\App
 start-process "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install" -Wait
 
 #Stop all transcription
