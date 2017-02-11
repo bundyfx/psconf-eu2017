58d8f39336501f9c228c3668637153a1a51000f9
Flynn Bundy <f.bundy@coolblue.nl>
2017-02-11 17:59:22 +0100

    t

--- developers-world/main.ps1
+++ developers-world/main.ps1
@@ -1,27 +1,38 @@
 powershell
 break
+
+Import-Module AWSPowerShell.NetCore
 Set-DefaulAwsRegion -Region 'eu-west-1'
 
-#ensure empty bucket
-Import-Module AWSPowerShell.NetCore
+########################
+###  Configuration   ###
+########################
 
 #See our newly put mofs.zip file
 Get-S3Object -BucketName powershell-dsc-mofs
 
+
+###################################
+###  Infrastructure Deployment  ###
+###################################
+
 #Create new cloudformation stack
 New-CFNStack -StackName Demo -TemplateURL "https://s3-eu-west-1.amazonaws.com/cf-templates-qaogskw029mf-eu-west-1/demo.yaml" -Region 'eu-west-1'
-
 Get-CFNStack -StackName Demo
+
+# 5 minutes
 
 #Collect the DSC Output from S3 if debugging required
 $DSCOutput = Get-S3Object -BucketName powershell-dsc-mofs
-$DSCOutput.Where{$Psitem.Key -match 'txt'} | % {Copy-S3Object -BucketName powershell-dsc-mofs -Key $Psitem.Key -LocalFile "/Users/bundyfx/git/psconf-eu2017/$($psitem.Key)"}
+$DSCOutput.Where{$Psitem.Key -match 'txt'} | Foreach-Object { Copy-S3Object -BucketName powershell-dsc-mofs -Key $Psitem.Key -LocalFile "/Users/bundyfx/git/psconf-eu2017/$($psitem.Key)" }
 
 Get-Childitem *.txt | Get-Content
 
+########################
+###  App Deployment  ###
+########################
 
-#Deployment
-$Deploy = New-CDDeployment -ApplicationName beertime -GitHubLocation_Repository 'bundyfx/psconf-eu2017-beertime' -GitHubLocation_CommitId '5bebf03e9df579c6a83d3a882fbac2ed632971e1' -DeploymentGroupName 'Production' -Revision_RevisionType Github
+$Deploy = New-CDDeployment -ApplicationName beertime -GitHubLocation_Repository 'bundyfx/psconf-eu2017-beertime' -GitHubLocation_CommitId '04b43b136bfa4ff1e349f8b0ada6f29949613159' -DeploymentGroupName 'Production' -Revision_RevisionType Github
 Get-CdDeployment $Deploy
 
 #beertime dns name from load balancer
